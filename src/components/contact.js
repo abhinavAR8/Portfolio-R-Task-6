b2a32c50cfa98ff514ea510e0d2260ea96e120f3
Abhinav nandan <109851249+abhinavAR8@users.noreply.github.com>
2023-10-15 20:02:24 +0530

    Add files via upload

---
+++ src/components/contact.js
@@ -0,0 +1,16 @@
+
+
+
+import React from 'react';
+
+function Contact() {
+  return (
+    <section className="contact">
+      <h2>Contact Me</h2>
+      <p>Email: abhinav10092002@gmail.com</p>
+      <p>Phone: 9031880422</p>
+    </section>
+  );
+}
+
+export default Contact;
